503a208e04459e296f86db108a815f15d9addbf8
RBalles <ramonballesteros1@gmail.com>
2023-06-06 19:02:02 +0200

    Subir Contador

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import rbLogo from './img/Logo-RB.png'
+import Boton from './components/Boton'
+import Contador from './components/Contador'
+import { useState } from 'react';
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);   
+  }
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+
+  return (
+    <div className="App">
+      <div className='rb-logo-contenedor'>
+        <img
+        className='rb-logo'
+        src={rbLogo} 
+        alt='logo RB' />
+      </div>
+      
+      <div className='contenedor-principal'>
+
+        <Contador numClics={numClics} />
+
+        <Boton 
+        texto='Click' 
+        esBotonDeClic={true} 
+        manejarClic={manejarClic} />
+        
+        <Boton 
+        texto='Reiniciar' 
+        esBotonDeClic={false} 
+        manejarClic={reiniciarContador} />
+        
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
